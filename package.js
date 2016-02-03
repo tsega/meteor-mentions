b602727eacad704a9805ef939dd22b8e38bddc11
Tsegaselassie Tadesse <tsegaselassi@gmail.com>
2016-02-03 19:04:01 +0300

    Added feature to query by nested object attribute. Related #3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'tsega:mentions',
-  version: '0.0.4',
+  version: '0.0.5',
   summary: '@metions for Meteor Apps',
   git: 'https://github.com/tsega/meteor-mentions',
   documentation: 'README.md'
